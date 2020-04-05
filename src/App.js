b34fcf99a08662a2fcbce3ed6200137fcc2a7037
Sushant Bansal <sushantbansal2@gmail.com>
2020-04-05 16:39:23 -0500

    Added Redux, Migrated Components to Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
+import M from "materialize-css";
 
 import SearchBar from "./Components/Layout/SearchBar";
 import Logs from "./Components/Logs/Logs";
+import AddBtn from "./Components/Layout/AddBtn";
+
+import AddLogModal from "./Components/Logs/AddLogModal";
+import EditLogModal from "./Components/Logs/EditLogModal";
+import AddTechModal from "./Components/Techs/AddTechModal";
+import TechListModal from "./Components/Techs/TechListModal";
+
+import { Provider } from "react-redux";
+import Store from "./store";
 
 function App() {
   // Initialize Material Design
@@ -12,12 +21,19 @@ function App() {
   }, []);
 
   return (
-    <Fragment>
-      <SearchBar></SearchBar>
-      <div className='container'>
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={Store}>
+      <Fragment>
+        <SearchBar></SearchBar>
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
